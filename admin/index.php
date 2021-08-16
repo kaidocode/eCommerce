b097de20e6fc620b184839e2b9edcf913f4f6d47
nacerkraa <nacer.kraa@gmail.com>
2021-08-17 00:54:50 +0200

    create main files

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+include "init.php";
+include $tpl . "header.php";
+
+?>
+
+
+Welcome To Index
+
+<i class="fas fa-home fa-5x"></i>
+
+<?php include $tpl . "footer.php"?>
---
+++ admin/init.php
@@ -0,0 +1,5 @@
+<?php 
+
+$tpl = "include/templetes/";
+$css = "layout/css/";
+$js = "layout/js/";
