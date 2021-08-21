b1dae9989d62e061a159dd6b77a2dbfb7dcebb18
nacerTec <nacer.kraa@univ-constantine2.dz>
2021-08-21 11:05:42 +0100

    Login form part_2

--- admin/include/templetes/header.php
+++ admin/include/templetes/header.php
@@ -6,7 +6,7 @@
     <title>Admin</title>
     <link rel="stylesheet" href= "<?php echo $css?>bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $css?>all.min.css">
-    <link rel="stylesheet" href="<?php echo $css?>backend.css">
-    <link href="/<?php echo $css?>backend.css" rel="stylesheet" type="text/css" />
+    <!-- <link rel="stylesheet" href="<?php echo $css?>backend.css"> -->
+    <link href="./<?php echo $css?>backend.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,35 @@
 <?php 
 
 include "init.php";
+include "connect.php";
 include "include/languages/english.php";
 include $tpl . "header.php";
 
 ?>
 
+<?php 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+        $hashpass = sha1($password);
+       
+        // cheak if user exist in db
 
-<form class="login">
+        $stmt = $con -> prepare("SELECT UserName, Password FROM users WHERE UserName = ? AND Password = ? AND groupId = 1 ");
+        $stmt -> execute(array($username, $hashpass));
+        $count = $stmt->rowCount();
+
+        if ($count>0) {
+            echo 'Welcome ' . $username;
+        }
+        
+    };
+?>
+
+<form class="login" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
 <h4 class="text-center">Admin Login</h4>
 <input class="form-control input-lg" type="text" name='user' placeholder="Username" autocomplete='off'>
-<input class="form-control input-lg" type="password" name='password' placeholder="Password" autocomplete='new-password'>
+<input class="form-control input-lg" type="password" name='pass' placeholder="Password" autocomplete='new-password'>
 <input class="btn btn-primary btn-block input-lg" type="submit" value="Login">
 </form>
 
