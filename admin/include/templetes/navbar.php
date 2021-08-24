7ea4ca92a40c648c1eb5ce900f0e76750878d759
nacerTec <nacer.kraa@univ-constantine2.dz>
2021-08-24 22:08:03 +0100

    Stayling Add New Members

--- admin/include/templetes/navbar.php
+++ admin/include/templetes/navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><?php echo langs('HOME') ?></a>
+      <a class="navbar-brand" href="index.php"><?php echo langs('HOME') ?></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -16,11 +16,11 @@
       <ul class="nav navbar-nav">
         <li><a href="#"><?php echo langs('CTEGORIES') ?></a></li>
         <li><a href="#"><?php echo langs('ITEMS') ?></a></li>
-        <li><a href="#"><?php echo langs('MEMBERS') ?></a></li>
+        <li><a href="members.php"><?php echo langs('MEMBERS') ?></a></li>
         <li><a href="#"><?php echo langs('STATISTICS') ?></a></li>
         <li><a href="#"><?php echo langs('LOGS') ?></a></li>
-       
-        
+
+
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
--- admin/members.php
+++ admin/members.php
@@ -10,8 +10,55 @@ if (isset($_SESSION['Username'])) {
 
     switch ($do) {
         case 'Manage':
-            echo "You are in Manage page ";
+            echo "You are in Manage page <br/>";
+            echo "<a href='?do=Add'>Add New Members</a>";
             break;
+        case 'Add':
+        ?>
+
+        <h1 class="text-center">Add New Members</h1>
+        <div class="container">
+        <form class="form-horizontal" action="?do=Insert" method="Post">
+
+            <!-- start username -->
+            <div class="form-group form-group-lg">
+                <label class="col-sm-2 control-label">User Name :</label>
+                <div class="col-sm-10 col-md-6">
+                    <input type="text"  name="username" class="form-control" autocomplete="off" required="required">
+                </div>
+            </div>
+            <!-- start password -->
+            <div class="form-group form-group-lg">
+                <label class="col-sm-2 control-label">Password :</label>
+                <div class="col-sm-10 col-md-6">
+
+                    <input type="password" name="password" class="form-control" autocomplete="new-password" required="required">
+                </div>
+            </div>
+            <!-- start email -->
+            <div class="form-group form-group-lg">
+                <label class="col-sm-2 control-label">Email :</label>
+                <div class="col-sm-10 col-md-6">
+                    <input type="email" name="email" class="form-control" required="required">
+                </div>
+            </div>
+            <!-- start fullame -->
+            <div class="form-group form-group-lg">
+                <label class="col-sm-2 control-label">Fullame :</label>
+                <div class="col-sm-10 col-md-6">
+                    <input type="text" name="fullname" class="form-control" required="required">
+                </div>
+            </div>
+            <!-- start submit -->
+            <div class="form-group form-group-lg">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <input type="submit" value="Add New Members" class="btn btn-primary btn-lg">
+                </div>
+            </div>
+        </form>
+        </div>
+        <?php
+        break;
         case 'Edit':
             $userId = isset($_GET['userId']) && is_numeric($_GET['userId']) && $_GET['userId'] == $_SESSION['Id'] ? intval($_GET['userId']) : 0 ;
             $stmt = $con -> prepare("SELECT * FROM users WHERE userId = ? LIMIT 1");
@@ -71,8 +118,69 @@ if (isset($_SESSION['Username'])) {
 
         break;
 
+        case 'Insert':
+        if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+            $User = $_POST['username'];
+            $Password = sha1($_POST['password']);
+            $Email = $_POST['email'];
+            $Name = $_POST['fullname'];
+
+
+            // Password Trick
+
+
+
+            // Validate The Form
+            $formErrors = array();
+
+            echo "<h1 class='text-center'>Add New Members</h1>";
+            echo '<div class="container">';
+
+
+
+            if (strlen($User) < 4) {
+              $formErrors[] = 'length cant be less than 4 character';
+            }
+
+            if (empty($User)) {
+              $formErrors[] = 'User Name Cant Be Empty';
+            }
+
+            if (strlen($_POST['password']) < 4 ) {
+              $formErrors[] = 'Password length cant be less than 8 character';
+            }
+
+
+
+            if (empty($Email)) {
+              $formErrors[] = 'Email Cant Be Empty';
+            }
+
+            if (empty($Name)) {
+              $formErrors[] = 'Full Name Cant Be Empty';
+            }
+
+            foreach ($formErrors as $error) {
+              echo '<div class="alert alert-danger">'.$error . '</div>';
+            }
+
+
+            if (empty($formErrors)) {
+              $stmt = $con -> prepare("INSERT INTO users (username,email,fullname,password) VALUES (?,?,?,?)");
+
+              $stmt -> execute(array($User, $Email, $Name, $Password));
+
+              echo '<div class="alert alert-success">'.$stmt->rowCount() . ' Record Added</div>';
+            }
+          }else {
+            echo "<h1 class='text-center'>Add New Members</h1>";
+              echo '<div class="alert alert-danger">Sorry You Cant brows This Page</div>';
+          }
+            echo "</div>";
+        break;
         case 'Update':
-            if ($_SERVER['REQUEST_METHOD']=='POST') {
+            if ($_SERVER['REQUEST_METHOD'] =='POST') {
 
                 $Id = $_POST['userid'];
                 $User = $_POST['username'];
@@ -119,12 +227,7 @@ if (isset($_SESSION['Username'])) {
                   echo '<div class="alert alert-success">'.$stmt->rowCount() . " Record Updated</div>";
                 }
 
-
-
-
-
-
-            } else {
+                } else {
                 echo "Sorry You Can't brows This Page ";
             }
             echo "</div>";
