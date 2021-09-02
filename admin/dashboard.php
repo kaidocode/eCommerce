94ae7edc32ba3e8a62e87af3523b58929e6e2c30
nacerTec <nacer.kraa@univ-constantine2.dz>
2021-09-02 15:11:34 +0100

    Stayling Dashboard Page

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['Username'])) {
     /* Start Dashboard */
     ?>
     
-    <div class="container home-stats">
+    <div class="container home-stats text-center">
         <h1>Dashboard</h1>
         <div class="row">
             <div class="col-md-3">
@@ -35,6 +35,32 @@ if (isset($_SESSION['Username'])) {
                 <div class="stat">
                     Total Comments
                     <span>1254</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container latest">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fa fa-users"></i> Latest Register Users
+                    </div>
+                    <div class="panel-body">
+                        Test
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fa fa-tag"></i> Latest Items
+                    </div>
+                    <div class="panel-body">
+                        Test
+                    </div>
                 </div>
             </div>
         </div>
