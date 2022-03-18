e704c24a62979749544bbaf71edf5223bf8059b9
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-03-18 18:23:10 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ create database shop;
 use shop;
 create table users(userId int(11) PRIMARY KEY AUTO_INCREMENT,Username VARCHAR(255),Password VARCHAR(255),Email VARCHAR(255),FullName VARCHAR(255),GroupId int(1) DEFAULT 0,TrustStatus int(1) DEFAULT 0,RegStatus int(1) DEFAULT 0,Date DATE);
 
-INSERT INTO users(username,password,email,fullname,groupid) values('nacer',sha1('12345678'),'nacer@gmail.com','issa',1);
+INSERT INTO users(username,password,email,fullname,groupid) values('nacer',sha1('123456789'),'nacer@gmail.com','issa',1);
 
 INSERT INTO users(username,password,email,fullname,groupid)values('ahmed',sha1('1234'),'ahmed@gmail.com','issa',0);
 
